a57e4616cece29110ea8178e92d5d942c00ffd25
sohagzayan <gdsohag360@gmail.com>
2023-12-31 18:58:10 +0600

    done the job search form with responsive

---
+++ app/(root)/find_job/page.tsx
@@ -0,0 +1,12 @@
+import JobSearchForm from '@/components/shared/job-search-form/JobSearchForm'
+import React from 'react'
+
+const FindJob = () => {
+    return (
+        <div>
+            <JobSearchForm />
+        </div>
+    )
+}
+
+export default FindJob
